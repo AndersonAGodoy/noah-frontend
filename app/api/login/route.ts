19f04601e03d9800ac8e4355f797d19c89778755
Anderson Godoy <anderson.a.godoy@gmail.com>
2025-04-23 16:24:56 -0300

    retira responsibilidade do front

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,33 +3,10 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const { email, password } = await request.json();
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    }
-  );
-
-  const data = await response.json();
-
-  if (response.ok) {
-    const res = NextResponse.json({ message: "Login successful" });
-    res.cookies.set("token", data.access_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      maxAge: 60 * 60 * 24, // 1 day
-      path: "/",
-    });
-    return res;
-  } else {
-    return NextResponse.json({ message: "Login failed" }, { status: 401 });
-  }
+  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+    credentials: "include", // ESSENCIAL para receber e enviar o cookie
+  });
 }
