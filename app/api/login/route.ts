88eefca4e97b3cfade0c63dcd83ea34cff314834
Anderson Godoy <anderson.a.godoy@gmail.com>
2025-04-24 17:07:58 -0300

    altera algumas coisa

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,19 +5,21 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const { email, password } = await request.json();
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include", // 👈 mantêm os cookies
-      body: JSON.stringify({ email, password }),
-    }
-  );
+  let response = NextResponse.next();
 
-  if (response.ok) {
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include", // 👈 mantêm os cookies
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (data.ok) {
+    const responseData = await data.json();
+    console.log("responseData", responseData);
+    response.cookies.set("token", responseData["access_token"]);
     return NextResponse.json({ message: "Login successful" });
   } else {
     return NextResponse.json({ message: "Login failed" }, { status: 401 });
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,8 @@
 import { Suspense } from "react";
 import DashboardPage from "../../components/DashboardPage";
 import DashboardSkeleton from "../../components/DashboardSkeleton";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const cookieStore = cookies();
-  const token = (await cookieStore).get("token")?.value;
-
-  if (!token) {
-    redirect("/login");
-  }
-
   return (
     <Suspense fallback={<DashboardSkeleton />}>
       <DashboardPage />
